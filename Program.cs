8446a728480f8db07079441963d6550bb1acc177
spinellim <mfelix-spinelli@hotmail.com>
2023-10-04 11:17:34 -0300

    Projeto POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia(numero:"123456", modelo: "Modelo 1", imei: "123123113",memoria: 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Zap zap");
+
+Iphone iphone = new Iphone(numero:"4545467", modelo: "Modelo 2", imei: "4457453",memoria: 32);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Zap zap 2");
+
